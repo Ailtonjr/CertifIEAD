cba9606823906ce902dd72f1f5313934868293c9
AiltonJr <ailtoncardoso0@gmail.com>
2016-07-28 12:19:01 -0300

    Adicionado classe que cria as conexões

---
+++ CertifIEAD/src/br/iead/certifiead/factory/ConnectionFactory.java
@@ -0,0 +1,26 @@
+package br.iead.certifiead.factory;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Ailton Cardoso Jr
+ */
+public class ConnectionFactory {
+    // Parametros de Conexão
+    private String url = "jdbc:postgresql://localhost:5432/db_certifiead";
+    private String usuario = "postgres";
+    private String senha = "123";
+    private Connection conexao;
+
+    // Retorna uma conexão com o BD
+    public Connection getConnection() {
+        try {
+            this.conexao = DriverManager.getConnection(url, usuario, senha);
+            return conexao;
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
